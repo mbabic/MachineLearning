e60e7eee4d7df568c1bb7f590ecb66f25d8030a3
mbabic <mbabic@ualberta.ca>
2013-12-13 11:13:24 -0700

    sigmod(), costFunction(), and gradient calculation implemented.

--- 2/mlclass-ex2/costFunction.m
+++ 2/mlclass-ex2/costFunction.m
@@ -11,22 +11,15 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 %
 % Note: grad should have the same dimensions as theta
 %
+J = (1 / m) * sum ( (-y) .* (log(sigmoid(X*theta))) - ... 
+	(ones(m, 1) - y) .* (log(ones(m, 1) - sigmoid(X*theta))) )
 
 
-
-
-
-
-
-
-% =============================================================
+grad = (1 / m) * (X' * (sigmoid(X*theta) - y))
 
 end
--- 2/mlclass-ex2/plotData.m
+++ 2/mlclass-ex2/plotData.m
@@ -5,24 +5,26 @@ function plotData(X, y)
 
 % Create New Figure
 figure; hold on;
-
-% ====================== YOUR CODE HERE ======================
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
-%
+
+% Find indices of positive and negative examples in the training set
+positive_results = find(y == 1);
+negative_results = find(y == 0);
+
+% Plot the examples
+% since positive_results is a vector of indices, the result of doing 
+% X(positive_results, 1) is to select the lx1 subvector of X where each row
+% corresponds to a row of the vector y in which the value 1 was stored and the
+% column is the first column of the matrix X
+
+plot(X(positive_results, 1), X(positive_results, 2), 'k+', 'LineWidth', 2, ... 
+	'MarkerSize', 7);
 
 
-
-
-
-
-
-
-
-% =========================================================================
-
-
+plot(X(negative_results, 1), X(negative_results, 2), 'ko', 'MarkerFaceColor',...
+	'y' , 'MarkerSize', 7);
 
 hold off;
 
