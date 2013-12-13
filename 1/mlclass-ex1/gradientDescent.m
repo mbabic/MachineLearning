1905cf8b5a531cfbbe32b44f9d2e02faea0939f2
mbabic <mbabic@ualberta.ca>
2013-12-12 20:30:27 -0700

    featureNoramlize() for optional exercise(s) implemented.

--- 1/mlclass-ex1/gradientDescent.m
+++ 1/mlclass-ex1/gradientDescent.m
@@ -3,21 +3,14 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 multiplier = alpha / m;
 for iter = 1:num_iters
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
 	temp_theta0 = theta(1) - multiplier * sum( X(:, 1)' * (X * theta - y));
 	temp_theta1 = theta(2) - multiplier * sum( X(:, 2)' * (X * theta - y)); 
 	theta = [temp_theta0; temp_theta1];
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
+   	J_history(iter) = computeCost(X, y, theta);
 
 end
 
