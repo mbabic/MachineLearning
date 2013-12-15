578e5751ed4e20cee7c009543e5072acc644c207
mbabic <mbabic@ualberta.ca>
2013-12-15 11:44:10 -0700

    lrCostFunction(), the vectorized implementation of the regularized cost function and gradient calculation, implemented and submitted.

--- 3/mlclass-ex3/lrCostFunction.m
+++ 3/mlclass-ex3/lrCostFunction.m
@@ -8,45 +8,21 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
-%
-%           sigmoid(X * theta)
-%
-%       Each row of the resulting matrix will contain the value of the
-%       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
-%
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
+J = 	(1 / m) * sum ( (-y) .* (log(sigmoid(X*theta))) - ... 
+	(ones(m, 1) - y) .* (log(ones(m, 1) - sigmoid(X*theta))) ) + ...
+	(lambda / (2 * m) ) * (sum(theta .^ 2) - theta(1)^2);
 
 
+% We store grad(1) into a temporary variable as theta_0 is not meant to be
+% regularized but we still want to implement the equation in a vectorized
+% way.
+grad = (1 / m) * (X' * (sigmoid(X*theta) - y));
+tmp = grad(1)
 
+% Now we regularize the gradient
+grad = grad + ((lambda / m) * theta)
 
-
-
-
-
-
-
-% =============================================================
-
-grad = grad(:);
+% Un-regularize the 0th gradient.
+grad(1) = tmp
 
 end
