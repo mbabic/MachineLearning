442bc3e4eea5bd3b94ead48fec2d92373a1bb274
mbabic <mbabic@ualberta.ca>
2013-12-17 14:05:38 -0700

    regularized linear regression cost function implemented and submitted

--- 5/mlclass-ex5/linearRegCostFunction.m
+++ 5/mlclass-ex5/linearRegCostFunction.m
@@ -12,26 +12,14 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
+% Calculate cost
+J = (1 / (2 * m)) * sum(((X * theta) - y) .^ 2);
+
+% Regularize cost
+J = J + ((lambda / (2 * m)) * sum(theta(2:end, 1) .^ 2));
 
 
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
+% Unroll the gradient.
 grad = grad(:);
 
 end
