813fbce3c74dea20ad529f0c2d939e2bd5b9dcf2
mbabic <mbabic@ualberta.ca>
2013-12-18 13:20:50 -0700

    computeCentroids() implemented and submitted.

--- 7/mlclass-ex7/computeCentroids.m
+++ 7/mlclass-ex7/computeCentroids.m
@@ -13,28 +13,32 @@ function centroids = computeCentroids(X, idx, K)
 % Useful variables
 [m n] = size(X);
 
-% You need to return the following variables correctly.
 centroids = zeros(K, n);
 
+% TODO: the implementation below is stupid.  We should only have to walk
+% the vector idx once to get all the information we need, but I don't have time
+% at the moment to figure out how to create arrays of structures in octave in
+% order to store the info as idx is walked.
+ 
+for k = 1:K
+	index_vec = zeros(0, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every centroid and compute mean of all points that
-%               belong to it. Concretely, the row vector centroids(i, :)
-%               should contain the mean of the data points assigned to
-%               centroid i.
-%
-% Note: You can use a for-loop over the centroids to compute this.
-%
+	% Find indices of data points assigned to centroid k and stores indices
+	% in index_vec.
+	for i = 1:m
+		if (idx(i) == k)
+			index_vec = [index_vec; i];
+		end
+	end
 
+	% We now proceed to calculate the mean of the data points associated
+	% with centroid k.
 
-
-
-
-
-
-
-% =============================================================
-
+	% We calculate the mean of each column (i.e., each feature) of X from
+	% each row (i.e., training example) which is assigned to centroid k.
+	means = mean(X(index_vec, :));
+	centroids(k, :) = means;
+end
 
 end
 
