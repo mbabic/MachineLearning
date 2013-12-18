b1be2abf7eae5ec4bc0adb6524ec4f647a49c798
mbabic <mbabic@ualberta.ca>
2013-12-17 22:03:57 -0700

    gaussian kernel calculation and optimum parameter selection functions implemented and submitted.

--- 6/mlclass-ex6/dataset3Params.m
+++ 6/mlclass-ex6/dataset3Params.m
@@ -7,28 +7,36 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C_vec = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0];
+sigma_vec = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+C_opt = C_vec(1);
+sigma_opt = sigma_vec(1);
+opt_err = realmax;
+for i = 1:length(sigma_vec)
+	sigma_test = sigma_vec(i);
+	for j = 1:length(C_vec)
+		C_test = C_vec(j);
+		% Train SVM on test C and sigma values.
+		model = svmTrain(X, y, C_test, @(x1, x2) ...
+		    gaussianKernel(x1, x2, sigma_test));
 
+		% Get predictions from trained SVM model on cross valuation
+		% data set.
+		predictions = svmPredict(model, Xval);
 
+		% Get prediction error.
+		err = mean(double(predictions ~= yval));
 
+		if err < opt_err
+			opt_err = err;
+			sigma_opt = sigma_test;
+			C_opt = C_test;
+		end 
+	end
+end	
 
-
-
-
-% =========================================================================
+sigma = sigma_opt;
+C = C_opt;
 
 end
